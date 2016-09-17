2617e109a10f4259123f7f05b061bb8ab55e2ee8
Dang Hung <danghung24031992@gmail.com>
2016-09-17 17:52:33 +0700

    init

---
+++ Final Project/PhotoViewer/GalleryViewController.swift
@@ -0,0 +1,170 @@
+
+/**
+ * PhotoViewer
+ * GalleryViewController
+ *
+ * Copyright (c) 2016 Nathan Blamires
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy 
+ * of this software and associated documentation files (the "Software"), to deal 
+ * in the Software without restriction, including without limitation the rights 
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies 
+ * of the Software, and to permit persons to whom the Software is furnished to do so, 
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all 
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE 
+ * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+import UIKit
+
+class GalleryViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    fileprivate var animationController: AnimationController = AnimationController()
+    var hideSelectedCell: Bool = false
+    
+    // view outlets
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    // data
+    var allPhotos: Array<Photo> = []
+    var selectedIndex: Int?
+    
+    // MARK: Setup Methods
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // generate photos
+        title = "Photo Viewer"
+        for x in 1...14 {
+            let image = UIImage(named:"\(x).jpg")
+            let title = "Photo \(x)"
+            let photo = Photo(title: title, image: image!)
+            allPhotos.append(photo)
+        }
+
+        // load collection
+        collectionView.reloadData()
+    }
+    
+    override var prefersStatusBarHidden : Bool {
+        return false
+    }
+    
+    override var preferredStatusBarStyle : UIStatusBarStyle {
+        return .lightContent
+    }
+    
+    // MARK: UICollectionViewDelegate, UICollectionViewDataSource
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: IndexPath) -> CGSize {
+        
+        let padding: CGFloat = 1
+        let minimum: CGFloat = 78
+
+        let itemsPerRow = floor(collectionView.frame.size.width / minimum)
+        let width = (collectionView.frame.size.width - ((itemsPerRow - 1)*padding)) / itemsPerRow
+        return CGSize(width: width, height: width)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ImageCell", for: indexPath) as! ImageCell
+        let photo = allPhotos[(indexPath as NSIndexPath).row]
+        cell.imageView.image = ((indexPath as NSIndexPath).row == selectedIndex && hideSelectedCell) ? nil : photo.image
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return allPhotos.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        selectedIndex = (indexPath as NSIndexPath).row
+        performSegue(withIdentifier: "FullScreenSegue", sender: nil)
+    }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        collectionView.collectionViewLayout.invalidateLayout()
+    }
+    
+    // MARK: Segue Methods
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destination = segue.destination as! PhotoViewController
+        destination.transitioningDelegate = self
+        destination.galleryDelegate = self
+        destination.setupWithPhotos(allPhotos, selectedPhotoIndex: selectedIndex!)
+    }
+}
+
+// MARK: UIViewControllerTransitioningDelegate
+
+extension GalleryViewController: GalleryDelegate {
+    func updateSelectedIndex(_ newIndex: Int){
+        selectedIndex = newIndex
+    }
+}
+
+protocol GalleryDelegate {
+    func updateSelectedIndex(_ newIndex: Int)
+}
+
+// MARK: UIViewControllerTransitioningDelegate
+
+// 1: Conforming to protocol
+extension GalleryViewController: UIViewControllerTransitioningDelegate {
+    
+    // 2: presentation controller
+    func animationController(forPresented presented: UIViewController,
+                                                   presenting: UIViewController,
+                                                                        source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        let photoViewController = presented as! PhotoViewController
+        animationController.setupImageTransition( image: allPhotos[selectedIndex!].image,
+                                                  fromDelegate: self,
+                                                  toDelegate: photoViewController)
+        return animationController
+    }
+    
+    // 3: dismissing controller
+    func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        let photoViewController = dismissed as! PhotoViewController
+        animationController.setupImageTransition( image: allPhotos[selectedIndex!].image,
+                                                  fromDelegate: photoViewController,
+                                                  toDelegate: self)
+        return animationController
+    }
+}
+
+// MARK: ImageTransitionProtocol
+
+extension GalleryViewController: ImageTransitionProtocol {
+    
+    // 1: hide selected cell for tranisition snapshot
+    func tranisitionSetup(){
+        hideSelectedCell = true
+        collectionView.reloadData()
+    }
+    
+    // 2: unhide selected cell after tranisition snapshot is taken
+    func tranisitionCleanup(){
+        hideSelectedCell = false
+        collectionView.reloadData()
+    }
+    
+    // 3: return window frame of selected image
+    func imageWindowFrame() -> CGRect{
+        let indexPath = IndexPath(row: selectedIndex!, section: 0)
+        let attributes = collectionView.layoutAttributesForItem(at: indexPath)
+        let cellRect = attributes!.frame
+        return collectionView.convert(cellRect, to: nil)
+    }
+}
---
+++ Final Project/PhotoViewer/PhotoViewController.swift
@@ -0,0 +1,254 @@
+
+/**
+ * PhotoViewer
+ * PhotoViewController
+ *
+ * Copyright (c) 2016 Nathan Blamires
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+ * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+import UIKit
+
+class PhotoViewController: UIViewController, UIScrollViewDelegate {
+    
+    // view outlets
+    @IBOutlet weak var controlsView: UIView!
+    @IBOutlet weak var exitButton: UIButton!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var moreButton: UIButton!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var contentView: UIView!
+    @IBOutlet weak var contentWidthConstraint: NSLayoutConstraint!
+    
+    // data
+    var allPhotoScrollViews: Array<UIScrollView> = []
+    var allPhotos: Array<Photo> = []
+    var currentPhotoIndex: Int = 0
+    
+    // delegate
+    var galleryDelegate: GalleryDelegate?
+    var statusBarHidden: Bool = false
+    var scrollViewDragging: Bool = false
+    
+    // MARK: Public Setup Methods
+    
+    func setupWithPhotos(_ photos: [Photo], selectedPhotoIndex: Int){
+        allPhotos = photos
+        currentPhotoIndex = selectedPhotoIndex
+    }
+
+    // MARK: Setup methods
+    
+    // adds styling and image views
+    override func viewDidLoad() {
+
+        updateTitle()
+        scrollView.delegate = self
+        
+        // add colours
+        view.backgroundColor = UIColor.black
+        titleLabel.textColor = UIColor.white
+        colourButton(exitButton)
+        colourButton(moreButton)
+        
+        // add shadows
+        addShadowToView(exitButton)
+        addShadowToView(moreButton)
+        addShadowToView(titleLabel)
+        
+        // add image views
+        setupImageViews()
+    }
+    
+    fileprivate func setupImageViews(){
+
+        // create all image views
+        var previousView: UIView = contentView
+        for x in 0...allPhotos.count-1 {
+            
+            let photo = allPhotos[x]
+            
+            // create sub scrollview
+            let subScrollView = UIScrollView()
+            subScrollView.delegate = self
+            contentView.addSubview(subScrollView)
+            allPhotoScrollViews.append(subScrollView)
+            
+            // create imageview
+            let imageView = UIImageView(image: photo.image)
+            imageView.contentMode = .scaleAspectFill
+            subScrollView.addSubview(imageView)
+            
+            // add subScrollView constraints
+            subScrollView.translatesAutoresizingMaskIntoConstraints = false
+            let attribute: NSLayoutAttribute = (x == 0) ? .leading : .trailing
+            scrollView.addConstraint(NSLayoutConstraint(item: subScrollView, attribute: .leading, relatedBy: .equal, toItem: previousView, attribute: attribute, multiplier: 1, constant: 0))
+            view.addConstraint(NSLayoutConstraint(item: subScrollView, attribute: .top, relatedBy: .equal, toItem: contentView, attribute: .top, multiplier: 1, constant: 0))
+            view.addConstraint(NSLayoutConstraint(item: subScrollView, attribute: .bottom, relatedBy: .equal, toItem: contentView, attribute: .bottom, multiplier: 1, constant: 0))
+            view.addConstraint(NSLayoutConstraint(item: subScrollView, attribute: .width, relatedBy: .equal, toItem: scrollView, attribute: .width, multiplier: 1, constant: 0))
+            
+            // add imageview constraints
+            imageView.translatesAutoresizingMaskIntoConstraints = false
+            subScrollView.addConstraint(NSLayoutConstraint(item: imageView, attribute: .width, relatedBy: .equal, toItem: imageView, attribute: .height, multiplier: (photo.image.size.width / photo.image.size.height), constant: 0))
+            subScrollView.addConstraint(NSLayoutConstraint(item: imageView, attribute: .centerX, relatedBy: .equal, toItem: subScrollView, attribute: .centerX, multiplier: 1, constant: 0))
+            subScrollView.addConstraint(NSLayoutConstraint(item: imageView, attribute: .centerY, relatedBy: .equal, toItem: subScrollView, attribute: .centerY, multiplier: 1, constant: 0))
+
+            // add imageview side constraints
+            for attribute: NSLayoutAttribute in [.top, .bottom, .leading, .trailing] {
+                let constraintLowPriority = NSLayoutConstraint(item: imageView, attribute: attribute, relatedBy: .equal, toItem: subScrollView, attribute: attribute, multiplier: 1, constant: 0)
+                let constraintGreaterThan = NSLayoutConstraint(item: imageView, attribute: attribute, relatedBy: .greaterThanOrEqual, toItem: subScrollView, attribute: attribute, multiplier: 1, constant: 0)
+                constraintLowPriority.priority = 750
+                subScrollView.addConstraints([constraintLowPriority,constraintGreaterThan])
+            }
+            
+            // set as previous
+            previousView = subScrollView
+        }
+        let xOffset = CGFloat(currentPhotoIndex) * scrollView.frame.size.width
+        scrollView.contentOffset = CGPoint(x: xOffset, y: 0)
+    }
+    
+    // ensure scroll view has correct content size when the view size changes
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        contentWidthConstraint.constant = CGFloat(allPhotos.count) * scrollView.frame.size.width
+        if !scrollViewDragging {
+            scrollView.contentOffset = CGPoint(x: CGFloat(currentPhotoIndex) * scrollView.frame.size.width, y: 0)
+        }
+    }
+
+    // MARK: Styling Methods
+    
+    fileprivate func colourButton(_ button: UIButton){
+        let tintableImage = button.backgroundImage(for: UIControlState())!.withRenderingMode(UIImageRenderingMode.alwaysTemplate)
+        button.setBackgroundImage(tintableImage, for: UIControlState())
+        button.tintColor = UIColor.white
+    }
+    
+    fileprivate func addShadowToView(_ view: UIView){
+        view.layer.masksToBounds = false
+        view.layer.shadowColor = UIColor.black.cgColor
+        view.layer.shadowOpacity = 0.7
+        view.layer.shadowOffset = CGSize(width: 2.0, height: 2.0)
+    }
+    
+    // MARK: UIScrollViewDelegate
+    
+    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+        if scrollView == self.scrollView {
+            scrollViewDragging = true
+        }
+    }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        if scrollView == self.scrollView {
+            scrollViewDragging = false
+        }
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        if scrollView == self.scrollView && scrollViewDragging {
+            currentPhotoIndex = getCurrentPageIndex()
+            updateTitle()
+        }
+    }
+    
+    // MARK: Utility Methods
+    
+    fileprivate func getCurrentPageIndex() -> Int {
+        return Int(round(scrollView.contentOffset.x / scrollView.frame.size.width))
+    }
+    
+    fileprivate func updateTitle(){
+        titleLabel.text = allPhotos[currentPhotoIndex].title
+    }
+
+    // MARK: Button Actions
+    
+    @IBAction func moreButtonSelected() {
+        // do something
+    }
+    
+    @IBAction func backButtonSelected() {
+        galleryDelegate?.updateSelectedIndex(currentPhotoIndex)
+        dismiss(animated: true, completion: nil)
+    }
+    
+    // MARK: Status Bar
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        statusBarHidden = true
+        setNeedsStatusBarAppearanceUpdate()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        statusBarHidden = false
+        setNeedsStatusBarAppearanceUpdate()
+    }
+    
+    override var prefersStatusBarHidden : Bool {
+        return statusBarHidden
+    }
+    
+    override var preferredStatusBarStyle : UIStatusBarStyle {
+        return .default
+    }
+}
+
+// MARK: ImageTransitionProtocol
+
+extension PhotoViewController: ImageTransitionProtocol {
+    
+    // 1: hide scroll view containing images
+    func tranisitionSetup(){
+        let photo = allPhotos[currentPhotoIndex]
+        titleLabel.text = photo.title
+        scrollView.isHidden = true
+    }
+    
+    // 2; unhide images and set correct image to be showing
+    func tranisitionCleanup(){
+        scrollView.isHidden = false
+        let xOffset = CGFloat(currentPhotoIndex) * scrollView.frame.size.width
+        scrollView.contentOffset = CGPoint(x: xOffset, y: 0)
+    }
+    
+    // 3: return the imageView window frame
+    func imageWindowFrame() -> CGRect{
+
+        let photo = allPhotos[currentPhotoIndex]
+        let scrollWindowFrame = scrollView.superview!.convert(scrollView.frame, to: nil)
+        
+        let scrollViewRatio = scrollView.frame.size.width / scrollView.frame.size.height
+        let imageRatio = photo.image.size.width / photo.image.size.height
+        let touchesSides = (imageRatio > scrollViewRatio)
+        
+        if touchesSides {
+            let height = scrollWindowFrame.size.width / imageRatio
+            let yPoint = scrollWindowFrame.origin.y + (scrollWindowFrame.size.height - height) / 2
+            return CGRect(x: scrollWindowFrame.origin.x, y: yPoint, width: scrollWindowFrame.size.width, height: height)
+        } else {
+            let width = scrollWindowFrame.size.height * imageRatio
+            let xPoint = scrollWindowFrame.origin.x + (scrollWindowFrame.size.width - width) / 2
+            return CGRect(x: xPoint, y: scrollWindowFrame.origin.y, width: width, height: scrollWindowFrame.size.height)
+        }
+    }
+}
